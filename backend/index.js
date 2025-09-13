6969e8a4ab6aec609b04847365925de43428184d
alalnseveroo <allanseveroo@gmail.com>
2025-09-12 22:39:42 -0300

    fix: ajusta configurações do Puppeteer e atualiza dependências

--- backend/index.js
+++ backend/index.js
@@ -67,8 +67,15 @@ const createWhatsappSession = (sessionId, socket) => {
         authStrategy: new LocalAuth({ clientId: sessionId }),
         puppeteer: {
           headless: true,
-          args: [ '--no-sandbox', '--disable-setuid-sandbox' ],
-          executablePath: require('puppeteer').executablePath(),
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--disable-gpu',
+            '--no-first-run'
+          ],
+          executablePath: '/usr/bin/chromium'
         }
     });
 
